f4eed6c175e7f88614007ab0bc3c93ed25ce3279
Maria Suzane <suzaane97@gmail.com>
2024-11-30 18:04:34 -0300

    possiveis correcoes

--- REACT/segundo_projeto/src/App.jsx
+++ REACT/segundo_projeto/src/App.jsx
@@ -1,13 +1,11 @@
-import Header from "./components/header/Header"
+import Header from "./components/header/Header"	
 import Footer from "./components/footer/Footer"
 import "./globalStyle/globalStyle.scss"
-
-export default function App() {
-  return (
+export default function App(){
+  return(
     <>
       <Header/>
       <Footer/>
     </>
-    
   )
 }
